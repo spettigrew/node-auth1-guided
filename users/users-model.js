fc23ef4bf0ab0e85787dc1f7178bb095e8319e16
spettigrew <sara.pettigrew5@gmail.com>
2020-01-14 19:02:07 -0700

    Notes added

--- users/users-model.js
+++ users/users-model.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")// bcrypt library
 const db = require("../database/dbConfig")
 
 function find() {
@@ -12,6 +13,9 @@ function findBy(filter) {
 }
 
 async function add(user) {
+  user.password = await bcrypt.hash(user.password, 12) 
+  // rounds is 2^ (to the power of) 10 === 1,024. request should take 1-2 seconds to complete.
+
   const [id] = await db("users")
     .insert(user)
  
