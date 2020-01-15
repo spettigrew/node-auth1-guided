449d098aecbe0d5bce3c0d279e94b3bdf10c006d
spettigrew <sara.pettigrew5@gmail.com>
2020-01-14 21:11:49 -0700

    Notes added

--- users/users-model.js
+++ users/users-model.js
@@ -13,8 +13,9 @@ function findBy(filter) {
 }
 
 async function add(user) {
+  // second parameter is the time complexity, not the number of rounds.
   user.password = await bcrypt.hash(user.password, 12) 
-  // rounds is 2^ (to the power of) 10 === 1,024. request should take 1-2 seconds to complete. Time hash is not sensitive at all 
+  // rounds is 2^ (to the power of) 10 === 1,024. request should take 1-2 seconds to complete. Time hash is not sensitive at all. Blowfish cipher; rounds is 2 to the power of the time complexity.
 
   const [id] = await db("users")
     .insert(user)
